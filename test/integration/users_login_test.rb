3f347e44f5a51bdc54f2f9f04c4bb69872d31006
AnjanaLP <anjana.lpn@gmail.com>
2022-02-19 19:45:13 +0000

    log in a user upon sign up

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -35,5 +35,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", user_path(@user)
     assert_select "a[href=?]", logout_path
+    assert is_logged_in?
   end
 end
