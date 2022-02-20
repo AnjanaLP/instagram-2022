9c3205d7cb17db616ff597d9f6383b37d4c6c518
AnjanaLP <anjana.lpn@gmail.com>
2022-02-20 10:31:22 +0000

    user must be logged in for user edit and update actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,39 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:bob)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
     assert_select 'title', full_title("Sign up")
+  end
+
+  test "should get show" do
+    get user_path(@user)
+    assert_response :success
+    assert_select 'title', full_title("#{@user.name} (@#{@user.username})")
+  end
+
+  test "should get edit when logged in" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_response :success
+    assert_select 'title', full_title("Edit profile")
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 end
