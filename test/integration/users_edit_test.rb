57de1dfa5f72b58ee9e827781c56fe9473e77dcd
AnjanaLP <anjana.lpn@gmail.com>
2022-02-20 10:52:28 +0000

    verify friendly forwarding only works on first log in attempt

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -17,8 +17,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.error-messages'
   end
 
-  test "successful edit" do
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
+    assert_not_nil session[:forwarding_url]
     log_in_as(@user)
     assert_redirected_to edit_user_url(@user)
     follow_redirect!
@@ -32,6 +33,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                               password: "" } }
     assert_not flash.empty?
     assert_redirected_to @user
+    assert_nil session[:forwarding_url]
     @user.reload
     assert_equal name,  @user.name
     assert_equal email, @user.email
